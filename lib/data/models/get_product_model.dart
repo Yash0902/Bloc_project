82d97e433329607d729d1c7a6117764ed7a82fd4
Yash Rathore <yash.rathore@yunicorn.in>
2025-10-28 00:41:33 +0530

    Bloc_project

---
+++ lib/data/models/get_product_model.dart
@@ -0,0 +1,54 @@
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable{
+  final int id;
+  final String title;
+  final String image;
+  final double price;
+  final bool isFavorite;
+  final int quantity;
+
+  Product({required this.id, required this.title, required this.image,
+    required this.price,required this.isFavorite,required this.quantity
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      image: json['image'],
+      price: json['price'],
+      isFavorite: json['isFavorite'],
+      quantity: json['quantity'],
+    );
+  }
+
+  Product copyWith({int? id,String? title,String? image,double? price,bool? isFavorite,int? quantity}) {
+    return Product(
+     id: id??this.id,
+      title: title??this.title,
+      image: image??this.image,
+      price: price??this.price,
+        isFavorite: isFavorite??this.isFavorite,
+      quantity: quantity??this.quantity
+
+    );
+  }
+
+  // use to request
+
+  // Map<String, dynamic> toJson() => {
+  //   'id': id,
+  //   'title': title,
+  //   'image': image,
+  //   'price': price,
+  //   'isFavorite': isFavorite,
+  //   'quantity': quantity,
+  // };
+
+
+
+  @override
+  List<Object?> get props => [id, title, image, price, isFavorite, quantity];
+
+}
